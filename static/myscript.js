1ae8d41f4fe59c07ccf18bd048e6c8ba5d02cb4e
Rawa08 <rawa_aref@hotmail.com>
2020-01-08 14:41:20 +0000

    layout fix

--- static/myscript.js
+++ static/myscript.js
@@ -1,4 +1,11 @@
+$(document).ready(function(){
 
 $( "#finalBuy" ).click(function() {
   alert( "This service is temporarily not available");
+});
+
+
+
+$('[data-toggle="tooltip"]').tooltip();
+
 });
